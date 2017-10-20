8746f1be1933a217da3ead3020a419e190fe3471
TSITCL\s152123 <hamu19970211@gmail.com>
2017-10-20 20:09:23 +0900

    ステージ作成

---
+++ Rokomoyo/Assets/Objects/DeathTrap/uGUIresize.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using System.Collections;
+
+public class uGUIresize : MonoBehaviour
+{
+    public  GameObject rawImage;
+    public ParticleSystem pe;
+    public GameObject thunderParticle;        //パーティクル
+    public Transform[] thunderPoints;         // 地点
+    float width, height;
+    // Use this for initialization
+    void Start()
+    {
+        width = 0;
+        height = 0;
+        pe.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            foreach (Transform explosionPos in thunderPoints)
+            {
+                GameObject thunder = Instantiate (thunderParticle,               // パーティクルオブジェクトの生成
+                    explosionPos.position, transform.rotation) as GameObject;
+                Destroy(thunder, 5f);                                             // ５秒後に消す
+            }
+        }
+        if (Input.GetKey("down"))
+        {
+               height += 1;
+        }
+        if (height < 10)
+        {
+            rawImage.transform.localScale = new Vector3(
+            gameObject.transform.localScale.x + height,
+            gameObject.transform.localScale.y + height,
+            gameObject.transform.localScale.z + height);
+        }
+       
+    }
+}
